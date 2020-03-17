784aa9795a2e3375d8d0bd3ccae3c7945075dfec
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-03-17 16:58:55 +0530

    First Name & Last Name Added for Super Admin

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -18,6 +18,8 @@ ActiveAdmin.register AdminUser do
 
   form do |f|
     f.inputs do
+      f.input :first_name
+      f.input :last_name
       f.input :email
       f.input :password
       f.input :password_confirmation
