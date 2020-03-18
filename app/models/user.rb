7723b3fc1dbcff73a2a9706b40e4a7f4c6247ca8
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-03-18 20:55:17 +0530

    Email Uniqness Added for Org Admin Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable #, :validatable
 
   enum role: [:admin, :participant]
 
@@ -12,10 +12,34 @@ class User < ApplicationRecord
   ## Validations
   validates :first_name, :last_name, :organization_id, presence: true
 
+  # From Devise module Validatable
   validates_presence_of :password, if: :password_required?
 
+  validates_presence_of   :email, if: :email_required?
+
+  validates_uniqueness_of :email,
+                          scope: [:organization_id],
+                          message: "Email already exists for this organization"
+
+  validates_format_of :email,
+                      with: /\A[^@]+@[^@]+\z/,
+                      allow_blank: true,
+                      if: :email_changed?
+
+  validates_confirmation_of :password, if: :confirmation_password_required?
+  validates_length_of       :password, within: 6..20, allow_blank: true
+
+
   def password_required?
-    # !persisted? || !password.nil? || !password_confirmation.nil?
-    confirmed? ? super : false
+    self.confirmed? ? confirmation_password_required? : false
+  end
+
+  def confirmation_password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
+
+  # From Devise module Validatable
+  def email_required?
+    true
   end
 end
