abd1bf17b9e86fcb7d79ece9b0eceb70a0873ca0
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-03-18 13:26:07 +0530

    TODO List Added to Validate Active Super Admins while Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  ## TODO: Allow Onlt Active Super Admins to LogIn
+
+  # # restrict access to admin module for non-admin users
+  # def authenticate_admin_user!
+  #   d = current_admin_user.try(:is_active?)
+  #   binding.pry
+  #   raise SecurityError unless current_admin_user.try(:is_active?)
+  # end
+  #
+  # rescue_from SecurityError do |exception|
+  #   binding.pry
+  #   redirect_to new_admin_user_session_url
+  # end
 end
