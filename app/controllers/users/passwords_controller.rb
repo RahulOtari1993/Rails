b83041780929d34a01c7e546dfbe4cb3d46d0410
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-03-19 18:42:50 +0530

    Forget Password & Reset Password Changes

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,34 +1,12 @@
 class Users::PasswordsController < Devise::PasswordsController
-  # append_before_action :assert_reset_token_passed, only: :edit
-
   # GET /resource/password/new
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource/password
   # def create
-  #   binding.pry
-  #
-  #
-  #
-  #   # self.resource = resource_class.send_reset_password_instructions(resource_params)
-  #   #
-  #   # unless successfully_sent?(resource)
-  #   #   Devise::Mailer.reset_password_instructions(resource).deliver
-  #   # end
-  #   #
-  #   # respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
-  #
-  #   # self.resource = resource_class.send_reset_password_instructions(resource_params)
-  #   # yield resource if block_given?
-  #   #
-  #   # if successfully_sent?(resource)
-  #   #   respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
-  #   # else
-  #   #   respond_with(resource)
-  #   # end
-  #
+  # super
   # end
 
   # GET /resource/password/edit?reset_password_token=abcdef
@@ -42,17 +20,6 @@ class Users::PasswordsController < Devise::PasswordsController
   # end
 
   protected
-
-  # Check if a reset_password_token is provided in the request
-  # def assert_reset_token_passed
-  #   if params[:reset_password_token].blank?
-  #
-  #     binding.pry
-  #
-  #     set_flash_message(:alert, :no_token)
-  #     redirect_to new_session_path(resource_name)
-  #   end
-  # end
 
   def after_resetting_password_path_for(resource)
     ## Create Entry in Org Admin Table
@@ -69,18 +36,10 @@ class Users::PasswordsController < Devise::PasswordsController
 
   # The path used after sending reset password instructions
   # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   binding.pry
   #   super(resource_name)
   # end
-  #
-  # def resource_params
-  #   # attributes = [:name, :location]
-  #   # attributes.push(:secrets) if current_user.admin?
-  #   #
-  #   # params.require(:organization).permit(attributes)
-  #
-  #   params
-  #                           .require(:user)
-  #                           .permit(:email, :password, :password_confirmation, :organization_id, :reset_password_token, :role)
-  # end
+
+  def resource_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :organization_id, :reset_password_token, :role)
+  end
 end
