3851ed76423b666b6baa49cfaeccd68f9e43565d
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-03-20 09:37:19 +0530

    Annotation Added for Models

--- app/models/campaign_user.rb
+++ app/models/campaign_user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: campaign_users
+#
+#  id          :bigint           not null, primary key
+#  user_id     :bigint
+#  campaign_id :bigint
+#  role        :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class CampaignUser < ApplicationRecord
   ## Associations
   belongs_to :user
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: submissions
+#
+#  id               :bigint           not null, primary key
+#  user_id          :bigint
+#  campaign_id      :bigint
+#  submissible_id   :integer
+#  submissible_type :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class Submission < ApplicationRecord
   ## Associations
   belongs_to :user
