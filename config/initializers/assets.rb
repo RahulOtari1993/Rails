f7d87d01be936301cdf192306a35d9487f78a22d
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-03-20 18:31:50 +0530

    Basic UI Setup for Org Admin Login

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w(super_admin/super_admin.css)
 
 Rails.application.config.assets.precompile += %w(super_admin/super_admin.js)
+Rails.application.config.assets.precompile += %w(libraries/bootstrap.min.css)
+Rails.application.config.assets.precompile += %w(libraries/material-design-iconic-font.min.css)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
