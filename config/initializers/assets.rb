5027ddb161297e5325b965f0cfe281480bdab825
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-03-23 19:27:58 +0530

    Assets Pipeline Configurations for Org Admin

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,13 +8,15 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w(super_admin/super_admin.css)
-
-Rails.application.config.assets.precompile += %w(super_admin/super_admin.js)
 Rails.application.config.assets.precompile += %w(libraries/bootstrap.min.css)
 Rails.application.config.assets.precompile += %w(libraries/material-design-iconic-font.min.css)
+Rails.application.config.assets.precompile += %w(super_admin/super_admin.css)
+Rails.application.config.assets.precompile += %w(organization_admin/organization_admin.css)
+
+Rails.application.config.assets.precompile += %w(super_admin/super_admin.js)
+Rails.application.config.assets.precompile += %w(organization_admin/organization_admin.js)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
+# folder are already added.Rails.application.config.assets.precompile += %w( organization_admin/organization_admin.js )
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
