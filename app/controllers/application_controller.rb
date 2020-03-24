dd5d01a69cacc9a4ab7709b8ee248d40ab761017
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-03-24 13:51:34 +0530

    Set Organization Based on the Domain Routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_organization
 
+  def set_organization
+    @organization ||= Organization.find_by_sub_domain(request.subdomain)
+  rescue ActiveRecord::RecordNotFound
+    raise ActionController::RoutingError.new("Unknown Organization: #{request.subdomain}")
+  end
 end
