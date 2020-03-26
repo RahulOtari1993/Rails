a53bddb0d21690f6bb81bfbba980aa7d1224121a
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-03-26 11:48:05 +0530

    Invite Org Admin Front-end Validation Added

--- app/assets/javascripts/organization_admin.js
+++ app/assets/javascripts/organization_admin.js
@@ -1,2 +1,28 @@
-console.log("JS Loaded!");
-
+$(document).ready(function() {
+  $('#new_organizations_user').validate({
+    rules: {
+      'organizations_user[first_name]': {
+        required: true
+      },
+      'organizations_user[last_name]': {
+        required: true
+      },
+      'organizations_user[email]': {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      'organizations_user[first_name]': {
+        required: 'Please enter user first name'
+      },
+      'organizations_user[last_name]': {
+        required: 'Please enter user last name'
+      },
+      'organizations_user[email]': {
+        required: 'Please enter user email',
+        email: 'Please enter valie email'
+      }
+    }
+  });
+});
