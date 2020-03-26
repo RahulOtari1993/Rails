9eb4637abc4d956e6e2386ea9b72525688e70f4c
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-03-26 12:30:31 +0530

    Login Page Front End Validation Added

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('.login_form').validate({
+    rules: {
+      'user[email]': {
+        required: true,
+        email: true
+      },
+      'user[password]': {
+        required: true
+      }
+    },
+    messages: {
+      'user[email]': {
+        required: 'Please enter user email',
+        email: 'Please enter valid user email'
+      },
+      'user[password]': {
+        required: 'Please enter user password'
+      }
+    }
+  });
+});
