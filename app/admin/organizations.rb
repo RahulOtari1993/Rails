8b0ddc73b4a2e556ae33ac0c89a028760e0b8211
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-03-30 16:35:40 +0530

    Update Campaign Domains if Org Domain get's Updated

--- app/admin/organizations.rb
+++ app/admin/organizations.rb
@@ -25,15 +25,42 @@ ActiveAdmin.register Organization do
       f.input :sub_domain, input_html: {required: true}
       f.input :is_active
       f.input :admin_user_id, as: :select,
-              collection: AdminUser.all.collect {|user| ["#{user.first_name} #{user.last_name}", user.id] },
+              collection: AdminUser.all.collect { |user| ["#{user.first_name} #{user.last_name}", user.id] },
               input_html: {required: true}
     end
     f.actions
   end
 
-  def destroy
-    resource.update!(is_deleted: true, deleted_by: current_admin_user.id)
-    redirect_to onboarding_organizations_path
-  end
+  controller do
+    def update(options = {}, &block)
+      super do |success, failure|
+        if success.present?
+          if resource.saved_change_to_sub_domain?
+            ## Only Update Sub Domains if Org's Domain is Updated
+            resource.campaigns.each do |campaign|
+              if campaign.domain_type == 'sub_domain'
+                sub_domain = "#{resource.sub_domain}.#{campaign.domain}"
+              else
+                sub_domain = "#{resource.sub_domain}#{campaign.domain}"
+              end
 
+              domain_list = DomainList.where(organization_id: resource.id, campaign_id: campaign.id).first
+              if domain_list.present?
+                domain_list.update(domain: sub_domain)
+              end
+            end
+          end
+        end
+
+        block.call(success, failure) if block
+        failure.html { render :edit }
+      end
+    end
+
+    def destroy
+      binding.pry
+      resource.update!(is_deleted: true, deleted_by: current_admin_user.id)
+      redirect_to onboarding_organizations_path
+    end
+  end
 end
