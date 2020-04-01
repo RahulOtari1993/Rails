a45635760d592663450b8e5977a039a998618641
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-01 19:47:14 +0530

    Assign Campaigns to a Newly Created Org Admin

--- app/models/organization_admin.rb
+++ app/models/organization_admin.rb
@@ -13,5 +13,15 @@ class OrganizationAdmin < ApplicationRecord
   belongs_to :admin, class_name: 'User', foreign_key: 'user_id'
   belongs_to :organization
 
+  ## Callbacks
+  after_create :assign_admins
+
   validates_uniqueness_of :user_id, :scope => :organization_id
+
+  ## Assign Campaigns to a Newly Created Org Admin
+  def assign_admins
+    self.organization.campaigns.each do |campaign|
+      campaign.campaign_users.create(user_id: self.user_id, role: 1)
+    end
+  end
 end
