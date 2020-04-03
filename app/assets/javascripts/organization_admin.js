28415d5c02237389e71ca1aba76e9c7ee776af0f
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-03 18:20:56 +0530

    List Actual Campaign Records

--- app/assets/javascripts/organization_admin.js
+++ app/assets/javascripts/organization_admin.js
@@ -62,7 +62,6 @@ $(document).ready(function() {
       {
         orderable: true,
         targets: 0,
-        checkboxes: { selectRow: true }
       }
     ],
     dom:
@@ -71,13 +70,13 @@ $(document).ready(function() {
       sLengthMenu: "_MENU_",
       sSearch: ""
     },
-    aLengthMenu: [[4, 10, 15, 20], [4, 10, 15, 20]],
+    aLengthMenu: [[5, 10, 15, 20], [5, 10, 15, 20]],
     select: {
       style: "multi"
     },
     order: [[1, "asc"]],
     bInfo: false,
-    pageLength: 4,
+    pageLength: 5,
     buttons: [
       {
         text: "<i class='feather icon-plus'></i> Add New Campaign",
@@ -90,13 +89,5 @@ $(document).ready(function() {
     initComplete: function(settings, json) {
       $(".dt-buttons .btn").removeClass("btn-secondary")
     }
-  });
-
-  dataListView.on('draw.dt', function(){
-    setTimeout(function(){
-      if (navigator.userAgent.indexOf("Mac OS X") != -1) {
-        $(".dt-checkboxes-cell input, .dt-checkboxes").addClass("mac-checkbox")
-      }
-    }, 50);
   });
 });
