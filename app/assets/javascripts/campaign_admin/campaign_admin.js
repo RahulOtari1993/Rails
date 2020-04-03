a71dfa8113921a2c70842ea3d3fdfc18dd2461b2
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-03 15:00:31 +0530

    Campaign Admin UI Layout Set up

---
+++ app/assets/javascripts/campaign_admin/campaign_admin.js
@@ -0,0 +1,28 @@
+// require jquery
+// require rails-ujs
+// require activestorage
+// require turbolinks
+// require jquery.validate
+// require jquery.validate.additional-methods
+// require ../organization_admin
+// require ../common_js
+
+// New Theme Integration
+// --------------------------------------------------------------
+
+//= require jquery
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+
+//= require vendors/vendors.min
+//= require vendors/charts/apexcharts.min
+//= require vendors/extensions/tether.min
+//= require vendors/extensions/shepherd.min.js
+
+//= require libraries/core/app-menu
+//= require libraries/scripts/components
+//= require libraries/scripts/pages/dashboard-analytics
+
+// This file do have some dependency ERRORS
+// require libraries/core/app
