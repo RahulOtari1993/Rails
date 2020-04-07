569b046daff4a8cba6d36286209ce4ae0c4d913f
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-07 13:01:01 +0530

    Settings Module Changes

--- app/assets/javascripts/campaign_admin.js
+++ app/assets/javascripts/campaign_admin.js
@@ -37,48 +37,21 @@ $(document).on('turbolinks:load', function() {
   });
 
   // Quill Editor Integration for Campaigns
-  var fullEditor = new Quill('.quill-editor-txt', {
-    bounds: '.quill-editor-txt',
+  var fullEditor = new Quill('.campaign-settings-editor', {
+    bounds: '.campaign-settings-editor',
     modules: {
       'formula': true,
       'syntax': true,
       'toolbar': [
-        [{
-          'font': []
-        }, {
-          'size': []
-        }],
+        [{'font': []}, {'size': []}],
         ['bold', 'italic', 'underline', 'strike'],
-        [{
-          'color': []
-        }, {
-          'background': []
-        }],
-        [{
-          'script': 'super'
-        }, {
-          'script': 'sub'
-        }],
-        [{
-          'header': '1'
-        }, {
-          'header': '2'
-        }, 'blockquote', 'code-block'],
-        [{
-          'list': 'ordered'
-        }, {
-          'list': 'bullet'
-        }, {
-          'indent': '-1'
-        }, {
-          'indent': '+1'
-        }],
-        ['direction', {
-          'align': []
-        }],
-        ['link', 'image', 'video', 'formula'],
+        [{ 'color': []}, {'background': []}],
+        [{'script': 'super'}, {'script': 'sub'}],
+        [{'header': '1'}, {'header': '2'}, 'blockquote', 'code-block'],
+        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+        ['direction', {'align': []}],['link', 'image', 'video', 'formula'],
         ['clean']
-      ],
+      ]
     },
     theme: 'snow'
   });
--- app/controllers/admin/campaigns/campaigns_controller.rb
+++ app/controllers/admin/campaigns/campaigns_controller.rb
@@ -9,7 +9,28 @@ class Admin::Campaigns::CampaignsController < ApplicationController
   end
 
   def update
-    binding.pry
+    respond_to do |format|
+      if @campaign.update(campaign_params)
+        if @campaign.domain_type == 'sub_domain'
+          sub_domain = "#{@organization.sub_domain}.#{@campaign.domain}"
+        else
+          sub_domain = "#{@organization.sub_domain}#{@campaign.domain}"
+        end
+
+        @domain_list = DomainList.where(organization_id: @organization.id, campaign_id: @campaign.id).first
+
+        if @domain_list.update(domain: sub_domain)
+          format.html { redirect_to edit_admin_campaign_path(@campaign), notice: 'Campaign was successfully updated.' }
+          format.json { render :edit, status: :updated }
+        else
+          format.html { render :edit }
+          format.json { render json: @domain_list.errors, status: :unprocessable_entity }
+        end
+      else
+        format.html { render :edit }
+        format.json { render json: @campaign.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
@@ -22,5 +43,10 @@ class Admin::Campaigns::CampaignsController < ApplicationController
   ## Set Campaign
   def set_campaign
     @campaign = Campaign.where(id: params[:id]).first
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def campaign_params
+    params.require(:campaign).permit(:name, :domain, :organization_id, :domain_type, :twitter, :rules)
   end
 end
