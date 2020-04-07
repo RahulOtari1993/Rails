f27ad357536cc53306d53611868619afc2a7715c
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-07 18:31:37 +0530

    Add General Title to Campaign Settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_071012) do
+ActiveRecord::Schema.define(version: 2020_04_07_125954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_071012) do
     t.boolean "templated"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "general_title"
     t.index ["organization_id"], name: "index_campaigns_on_organization_id"
   end
 
