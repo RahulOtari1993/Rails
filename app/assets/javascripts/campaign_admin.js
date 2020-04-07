57e1879219c5cbf13a30b2745978dfb4fca02b77
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-07 16:02:58 +0530

    Editor Added in Privacy Policy Text Area

--- app/assets/javascripts/campaign_admin.js
+++ app/assets/javascripts/campaign_admin.js
@@ -1,4 +1,9 @@
 $(document).on('turbolinks:load', function() {
+  // Fonts COnfig for Quill Editor
+  var Font = Quill.import('formats/font');
+  Font.whitelist = ['sofia', 'slabo', 'roboto', 'inconsolata', 'ubuntu'];
+  Quill.register(Font, true);
+
   // Add Campaign Form Validation
   $('.edit-campaign-form').validate({
     errorElement: 'span',
@@ -36,28 +41,37 @@ $(document).on('turbolinks:load', function() {
     }
   });
 
-  // Quill Editor Integration for Campaigns
-  var fullEditor = new Quill('.campaign-settings-editor', {
-    bounds: '.campaign-settings-editor',
-    modules: {
-      'formula': true,
-      'syntax': true,
-      'toolbar': [
-        [{'font': []}, {'size': []}],
-        ['bold', 'italic', 'underline', 'strike'],
-        [{ 'color': []}, {'background': []}],
-        [{'script': 'super'}, {'script': 'sub'}],
-        [{'header': '1'}, {'header': '2'}, 'blockquote', 'code-block'],
-        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-        ['direction', {'align': []}],['link', 'image', 'video', 'formula'],
-        ['clean']
-      ]
-    },
+  // Quill Editor Toolbar Config
+  var toolbar = {
+    'formula': true,
+    'syntax': true,
+    'toolbar': [
+      [{'font': []}, {'size': []}],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ 'color': []}, {'background': []}],
+      [{'script': 'super'}, {'script': 'sub'}],
+      [{'header': '1'}, {'header': '2'}, 'blockquote', 'code-block'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['direction', {'align': []}],['link', 'image', 'video', 'formula'],
+      ['clean']
+    ]
+  };
+
+  // Quill Editor Integration for Campaign Rules
+  var fullEditor = new Quill('.campaign-rules-editor', {
+    modules: toolbar,
+    theme: 'snow'
+  });
+
+  // Quill Editor Integration for Campaign Privacy Policy
+  var fullEditor = new Quill('.campaign-privacy-policy-editor', {
+    modules: toolbar,
     theme: 'snow'
   });
 
   // Add Rules Details of Quill Editor to Campaign Rules
   $('.edit-campaign-form').on('submit', function () {
-    $('.rules-txt-area').val($('.campaign-settings-editor .ql-editor').html());
+    $('.rules-txt-area').val($('.campaign-rules-editor .ql-editor').html());
+    $('.privacy-txt-area').val($('.campaign-privacy-policy-editor .ql-editor').html());
   });
 });
