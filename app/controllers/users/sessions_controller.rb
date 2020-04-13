fd2966a5e584f6df745db25007f622ce9a3c1c81
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-13 18:34:48 +0530

    User Sign put Redirection Changes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -40,6 +40,11 @@ class Users::SessionsController < Devise::SessionsController
     end
   end
 
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource)
+    request.referrer
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
