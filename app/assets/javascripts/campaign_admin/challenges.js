55791f8a0bed7e7c8b048285d699e4df8ca68dea
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-14 20:30:18 +0530

    Pill Component Loaded

--- app/assets/javascripts/campaign_admin/challenges.js
+++ app/assets/javascripts/campaign_admin/challenges.js
@@ -25,63 +25,63 @@ $(document).on('turbolinks:load', function() {
     }
   });
 
-  $('.challenge-wizard').validate({
-    errorElement: 'span',
-    rules: {
-      'challenge[mechanism]': {
-        required: true
-      },
-      'challenge[name]': {
-        required: true
-      },
-      'challenge[link]': {
-        required: true,
-        url: true
-      },
-      'challenge[description]': {
-        required: true
-      },
-      // 'hk_s3_1': {
-      //   required: true,
-      //   minlength: 8
-      // },
-      // 'hk_s4_1': {
-      //   required: true
-      // },
-      // 'hk_s4_2': {
-      //   required: true
-      // },
-      // 'hk_s5_1': {
-      //   required: true
-      // }
-    },
-    messages: {
-      'challenge[mechanism]': {
-        required: 'Please select challenge type'
-      },
-      'challenge[name]': {
-        required: 'Please enter challenge name'
-      },
-      'challenge[link]': {
-        required: 'Please enter link to be shared',
-        url: 'Please enter valid link'
-      },
-      'challenge[description]': {
-        required: 'Please enter challenge description'
-      },
-      // 'hk_s3_1': {
-      //   required: 'Please enter new password',
-      //   minlength: 'Password is too short (minimum is 8 characters)'
-      // },
-      // 'hk_s4_1': {
-      //   required: 'Please enter first name'
-      // },
-      // 'hk_s4_2': {
-      //   required: 'Please enter last name'
-      // },
-      // 'hk_s5_1': {
-      //   required: 'Please enter email address'
-      // }
-    }
-  });
+  // $('.challenge-wizard').validate({
+  //   errorElement: 'span',
+  //   rules: {
+  //     'challenge[mechanism]': {
+  //       required: true
+  //     },
+  //     'challenge[name]': {
+  //       required: true
+  //     },
+  //     'challenge[link]': {
+  //       required: true,
+  //       url: true
+  //     },
+  //     'challenge[description]': {
+  //       required: true
+  //     },
+  //     // 'hk_s3_1': {
+  //     //   required: true,
+  //     //   minlength: 8
+  //     // },
+  //     // 'hk_s4_1': {
+  //     //   required: true
+  //     // },
+  //     // 'hk_s4_2': {
+  //     //   required: true
+  //     // },
+  //     // 'hk_s5_1': {
+  //     //   required: true
+  //     // }
+  //   },
+  //   messages: {
+  //     'challenge[mechanism]': {
+  //       required: 'Please select challenge type'
+  //     },
+  //     'challenge[name]': {
+  //       required: 'Please enter challenge name'
+  //     },
+  //     'challenge[link]': {
+  //       required: 'Please enter link to be shared',
+  //       url: 'Please enter valid link'
+  //     },
+  //     'challenge[description]': {
+  //       required: 'Please enter challenge description'
+  //     },
+  //     // 'hk_s3_1': {
+  //     //   required: 'Please enter new password',
+  //     //   minlength: 'Password is too short (minimum is 8 characters)'
+  //     // },
+  //     // 'hk_s4_1': {
+  //     //   required: 'Please enter first name'
+  //     // },
+  //     // 'hk_s4_2': {
+  //     //   required: 'Please enter last name'
+  //     // },
+  //     // 'hk_s5_1': {
+  //     //   required: 'Please enter email address'
+  //     // }
+  //   }
+  // });
 });
