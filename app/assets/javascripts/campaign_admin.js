01de5dcd431d3da3bb77b8721b2cae6b276ee810
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-14 12:42:28 +0530

    Wizard Front End Validation Steps Added

--- app/assets/javascripts/campaign_admin.js
+++ app/assets/javascripts/campaign_admin.js
@@ -180,7 +180,9 @@ $(document).on('turbolinks:load', function() {
     $(".dt-checkboxes-cell input, .dt-checkboxes").addClass("mac-checkbox")
   }
 
-  $(".challenge-wizard").steps({
+  var form = $(".challenge-wizard");
+
+  $('.challenge-wizard').steps({
     headerTag: "h6",
     bodyTag: "fieldset",
     transitionEffect: "fade",
@@ -188,8 +190,81 @@ $(document).on('turbolinks:load', function() {
     labels: {
       finish: 'Submit'
     },
+    onStepChanging: function (event, currentIndex, newIndex) {
+      // Allways allow previous action even if the current form is not valid!
+      if (currentIndex > newIndex) {
+        return true;
+      }
+
+      return form.valid();
+    },
+    onFinishing: function (event, currentIndex) {
+      return form.valid();
+    },
     onFinished: function (event, currentIndex) {
-      alert("Form submitted.");
+      alert("Submitted!");
+    }
+  });
+
+  $('.challenge-wizard').validate({
+    errorElement: 'span',
+    rules: {
+      'hk_s1_1': {
+        required: true
+      },
+      'hk_s1_2': {
+        required: true
+      },
+      'hk_s2_1': {
+        required: true,
+        email: true
+      },
+      'hk_s2_2': {
+        required: true,
+        email: true
+      },
+      'hk_s3_1': {
+        required: true,
+        minlength: 8
+      },
+      'hk_s4_1': {
+        required: true
+      },
+      'hk_s4_2': {
+        required: true
+      },
+      'hk_s5_1': {
+        required: true
+      },
+    },
+    messages: {
+      'hk_s1_1': {
+        required: 'Please enter first name'
+      },
+      'hk_s1_2': {
+        required: 'Please enter last name'
+      },
+      'hk_s2_1': {
+        required: 'Please enter email address',
+        email: 'Please enter valid email address'
+      },
+      'hk_s2_2': {
+        required: 'Please enter confirm email address',
+        email: 'Please enter valid email address'
+      },
+      'hk_s3_1': {
+        required: 'Please enter new password',
+        minlength: 'Password is too short (minimum is 8 characters)'
+      },
+      'hk_s4_1': {
+        required: 'Please enter first name'
+      },
+      'hk_s4_2': {
+        required: 'Please enter last name'
+      },
+      'hk_s5_1': {
+        required: 'Please enter email address'
+      }
     }
   });
 });
