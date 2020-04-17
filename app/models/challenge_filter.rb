2f6bd4b6841e9ddb4cc284a691dda0667e53eccc
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-17 15:52:24 +0530

    Challenge FIlter COnstants Added & Backend Validation

--- app/models/challenge_filter.rb
+++ app/models/challenge_filter.rb
@@ -14,5 +14,12 @@ class ChallengeFilter < ApplicationRecord
 
   ## Associations
   belongs_to :challenge
-  
+
+  ## Constants
+  EVENTS = %w(Age Tags Gender Points Rewards Platforms)
+  AGE_CONDITIONS = %w(Equals Greater\ Than Less\ Than Greater\ Than\ or\ Equal Less\ Than\ or\ Equal)
+  SOCIAL_PLATFORMS = %w(Facebook Twitter Google+ Instagram Youtube)
+
+  ## Validations
+  validates :challenge_event, :challenge_condition, :challenge_value, presence: true
 end
