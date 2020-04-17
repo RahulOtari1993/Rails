a97c47fd940374af7c36c008921e2af606f2564c
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-17 10:10:12 +0530

    Model Annotation Updates

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,27 +2,31 @@
 #
 # Table name: challenges
 #
-#  id          :bigint           not null, primary key
-#  campaign_id :bigint
-#  name        :text
-#  platform_id :integer
-#  start       :datetime
-#  finish      :datetime
-#  timezone    :string
-#  points      :integer
-#  parameters  :string
-#  mechanism   :string
-#  feature     :boolean
-#  creator_id  :integer
-#  approver_id :integer
-#  content     :text
-#  link        :text
-#  clicks      :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  description :text
-#  reward_type :integer
-#  reward_id   :bigint
+#  id                 :bigint           not null, primary key
+#  campaign_id        :bigint
+#  name               :text
+#  platform           :integer
+#  start              :datetime
+#  finish             :datetime
+#  timezone           :string
+#  points             :integer
+#  parameters         :string
+#  mechanism          :string
+#  feature            :boolean
+#  creator_id         :integer
+#  approver_id        :integer
+#  content            :text
+#  link               :text
+#  clicks             :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  description        :text
+#  reward_type        :integer
+#  reward_id          :bigint
+#  is_draft           :boolean          default("true")
+#  image              :string
+#  social_title       :string
+#  social_description :string
 #
 class Challenge < ApplicationRecord
   #TODO : While Approving a Challenge, Check if ORG do have Social Media Config Available
--- app/models/organization_config.rb
+++ app/models/organization_config.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: organization_configs
+#
+#  id                   :bigint           not null, primary key
+#  organization_id      :bigint
+#  facebook_app_id      :string
+#  facebook_app_secret  :string
+#  google_client_id     :string
+#  google_client_secret :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
 class OrganizationConfig < ApplicationRecord
   belongs_to :organization
 end
