a7e60076ffa440d17be4822dc5cb5b5ee072b046
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-04-23 11:42:55 +0530

    Model Annotations Added

--- app/models/participant_account_connect.rb
+++ app/models/participant_account_connect.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: participant_account_connects
+#
+#  id             :bigint           not null, primary key
+#  participant_id :bigint
+#  email          :string
+#  token          :string
+#  platform       :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class ParticipantAccountConnect < ApplicationRecord
 end
