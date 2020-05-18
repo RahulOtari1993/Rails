7aa54ebd1bc8f27f6c7a3ad084e8f240413650f2
Hardik Kanjariya <hardik@bigkittylabs.com>
2020-05-18 20:36:59 +0530

    Back-end Validation for Profile Attributes

--- app/models/profile_attribute.rb
+++ app/models/profile_attribute.rb
@@ -5,4 +5,8 @@ class ProfileAttribute < ApplicationRecord
 
   ## ENUM
   enum field_type: {string: 0, text_area: 1, boolean: 2, date: 3, time: 4, date_time: 5, number: 6, decimal: 7}
+
+  ## Validations
+  validates :attribute_name, :display_name, :field_type, presence: true
+  validates :attribute_name, uniqueness: true
 end
