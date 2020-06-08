b888839f38f578ec2a560d1db2c307694a42bee9
Hardik Kanjariya <hardik@perksocial.com>
2020-06-08 15:35:14 +0530

    UID & Provider Columns added in Participants

--- db/migrate/20200608083512_add_omniauth_to_participants.rb
+++ db/migrate/20200608083512_add_omniauth_to_participants.rb
@@ -2,5 +2,7 @@ class AddOmniauthToParticipants < ActiveRecord::Migration[5.2]
   def change
     add_column :participants, :oauth_token, :string
     add_column :participants, :oauth_expires_at, :datetime
+    add_column :participants, :provider, :string
+    add_column :participants, :uid, :string
   end
 end
