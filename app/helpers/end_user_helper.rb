a64885a8da7fcf5a00dcab6767ed59122aeec99d
Hardik Kanjariya <hardik@perksocial.com>
2020-06-10 17:45:08 +0530

    SignUp/SignIn Button Conditional UI Added

---
+++ app/helpers/end_user_helper.rb
@@ -0,0 +1,16 @@
+module EndUserHelper
+  ## Display SignUp / SignIn with Google Button Based on Configured Challenge
+  def display_google_button
+    true
+  end
+
+  ## Display SignUp / SignIn with Facebook Button Based on Configured Challenge
+  def display_facebook_button
+    false
+  end
+
+  ## Display SignUp / SignIn with Email Button Based on Configured Challenge
+  def display_email_button
+    true
+  end
+end
