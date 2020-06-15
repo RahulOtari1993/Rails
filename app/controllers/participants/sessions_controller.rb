76dba89fdb3454b391046df0f0b9ec59315e9a1c
Ranga_perk <ranga@perksocial.com>
2020-06-15 13:16:17 +0530

    Modified code for showing errors for participant login

--- app/controllers/participants/sessions_controller.rb
+++ app/controllers/participants/sessions_controller.rb
@@ -13,24 +13,25 @@ class Participants::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  def create
-    org_participant = @organization.participants.find_by(:email => params[:participant][:email])
-    campaign_participant = @campaign.participants.where(:id => org_participant.id)
-    if org_participant.present? && campaign_participant.present?
-
-      self.resource = warden.authenticate!(auth_options)
-      set_flash_message!(:notice, :signed_in)
-      sign_in(resource_name, resource)
-      yield resource if block_given?
-      redirect_to participant_dashboard_path
-
-    elsif org_participant.present?
-      @campaign.participants << org_participant
-      redirect_to new_participant_session_path(resource)
-    else
-      redirect_to new_participant_session_path(resource) #new_participant_session_path(resource)
-    end
-  end
+  # def create
+    # org_participant = @organization.participants.find_by(:email => params[:participant][:email])
+    # campaign_participant = @campaign.participants.where(:id => org_participant.id) if org_participant.blank?
+    #
+    # if org_participant.present? && campaign_participant.present?
+    #
+    #   self.resource = warden.authenticate!(auth_options)
+    #   set_flash_message!(:notice, :signed_in)
+    #   sign_in(resource_name, resource)
+    #   yield resource if block_given?
+    #   redirect_to participant_dashboard_path
+    #
+    # elsif org_participant.present?
+    #   @campaign.participants << org_participant
+    #   redirect_to new_participant_session_path(resource)
+    # else
+    #   redirect_to new_participant_session_path(resource) #new_participant_session_path(resource)
+    # end
+  # end
 
   # DELETE /resource/sign_out
   # def destroy
