47a096316745d7716d773cbd64b7d99b63463bfb
Hardik Kanjariya <hardik@perksocial.com>
2020-06-23 14:07:11 +0530

    Create a Commit Log for Participant Login

--- app/controllers/participants/sessions_controller.rb
+++ app/controllers/participants/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Participants::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
+  after_action :after_login, :only => :create
 
   ## Configure End User Layout
   # layout 'end_user'
@@ -14,28 +15,6 @@ class Participants::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   # def create
-  #   Rails.logger.info "======== IN @organization ---> #{@organization.inspect}"
-  #   Rails.logger.info "======== IN EMAIL ---> #{params[:participant][:email].inspect}"
-  #   org_participant = @organization.participants.find_by(:email => params[:participant][:email])
-  #   Rails.logger.info "======== IN org_participant ---> #{org_participant.inspect}"
-  #
-  #   campaign_participant = @campaign.participants.where(:id => org_participant.id)
-  #   Rails.logger.info "======== IN campaign_participant ---> #{campaign_participant.inspect}"
-  #
-  #   if org_participant.present? && campaign_participant.present?
-  #
-  #     self.resource = warden.authenticate!(auth_options)
-  #     set_flash_message!(:notice, :signed_in)
-  #     sign_in(resource_name, resource)
-  #     yield resource if block_given?
-  #     redirect_to root_path
-  #
-  #   elsif org_participant.present?
-  #     @campaign.participants << org_participant
-  #     redirect_to new_participant_session_path(resource)
-  #   else
-  #     redirect_to new_participant_session_path(resource) #new_participant_session_path(resource)
-  #   end
   # end
 
   # DELETE /resource/sign_out
@@ -58,5 +37,12 @@ class Participants::SessionsController < Devise::SessionsController
   ## Pass Additional Param for Opening Popup of Onboarding Questions
   def after_sign_in_path_for(resource)
     root_path(:c => 1)
+  end
+
+  ## Create Login Entry for Participant
+  def after_login
+    log = ParticipantAction.new({participant_id: resource.id, points: 0, action_type: 'sign_in',
+                                 title: 'Signed in', user_agent: request.user_agent, ip_address: request.remote_ip})
+    log.save
   end
 end
