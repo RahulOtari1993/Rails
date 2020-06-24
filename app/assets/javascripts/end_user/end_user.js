30aac1fd3c0ff8ddcfc6d9959feae32673648ecb
Hardik Kanjariya <hardik@perksocial.com>
2020-06-24 19:02:13 +0530

    DateTime Picker Icon Placment issue solved

--- app/assets/javascripts/end_user/end_user.js
+++ app/assets/javascripts/end_user/end_user.js
@@ -9,10 +9,6 @@
 //= require jquery.validate
 //= require jquery.validate.additional-methods
 //= require vendors/extensions/sweetalert2.all.min
-//= require vendors/pickers/pickadate/picker
-//= require vendors/pickers/pickadate/picker.date
-//= require vendors/pickers/pickadate/picker.time
-//= require vendors/pickers/pickadate/legacy
 //= require moment
 //= require ../end_user/libraries/bootstrap-datetimepicker.min
 //= require ../main
