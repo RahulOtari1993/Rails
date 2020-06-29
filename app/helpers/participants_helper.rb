a985751b12651ef791fa22770910f49786723f93
Nitesh Thacker <nitesh@bigkittylabs.com>
2020-06-29 16:12:35 +0530

    Display % of Users Targeted in Challenge Overlay

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -8,4 +8,13 @@ module ParticipantsHelper
   def all_rewards_filter
     @campaign.rewards.pluck(:name, :id)
   end
+
+  ## Display % of Targeted Users
+  def targeted_users_percentage
+    total_participants = @challenge.completions
+    targeted_participants = @challenge.targeted_participants
+    percentage = (targeted_participants.count * 100 ) / total_participants.to_i
+
+    percentage
+  end
 end
