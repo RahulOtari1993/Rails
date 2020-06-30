7e376679400c42f0c933b26ce46a736f79e5f9a3
Hardik Kanjariya <hardik@perksocial.com>
2020-06-30 19:17:04 +0530

    Save Question Sequence in Database

--- app/controllers/admin/campaigns/challenges_controller.rb
+++ app/controllers/admin/campaigns/challenges_controller.rb
@@ -206,7 +206,7 @@ class Admin::Campaigns::ChallengesController < Admin::Campaigns::BaseController
                                                       challenge_filters_attributes: [:id, :challenge_id, :challenge_event,
                                                                                      :challenge_condition, :challenge_value],
                                                       :questions_attributes => [:id, :challenge_id, :category, :title, :is_required, :answer_type,
-                                                                                :profile_attribute_id, :placeholder, :additional_details,
+                                                                                :profile_attribute_id, :placeholder, :additional_details, :sequence,
                                                                                 :question_options_attributes => [:id, :question_id, :details, :answer]])
 
     ## Manage End Date, If not present add 500 Years in Start Date and Create a new End Date
@@ -370,7 +370,8 @@ class Admin::Campaigns::ChallengesController < Admin::Campaigns::BaseController
             profile_attribute_id: answer_type[1],
             placeholder: c_param[:placeholder],
             additional_details: c_param[:additional_details],
-            question_options_attributes: option_params
+            question_options_attributes: option_params,
+            sequence: c_param[:sequence]
         }
 
         if c_param.has_key?('id')
