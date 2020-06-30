aeb3e538d14b7309c89e71c8a264bb8ce749402e
Hardik Kanjariya <hardik@perksocial.com>
2020-06-30 18:48:24 +0530

    Question & Question Option Sequence Columns Added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_121829) do
+ActiveRecord::Schema.define(version: 2020_06_30_131715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -382,6 +382,7 @@ ActiveRecord::Schema.define(version: 2020_06_29_121829) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "answer"
+    t.integer "sequence"
     t.index ["question_id"], name: "index_question_options_on_question_id"
   end
 
@@ -396,6 +397,7 @@ ActiveRecord::Schema.define(version: 2020_06_29_121829) do
     t.datetime "updated_at", null: false
     t.string "placeholder"
     t.string "additional_details"
+    t.integer "sequence"
     t.index ["challenge_id"], name: "index_questions_on_challenge_id"
   end
 
