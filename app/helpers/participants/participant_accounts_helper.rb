a02babab37d7ae3f04deceff201e93cb65325d4d
Ranga_perk <ranga@perksocial.com>
2020-07-03 11:37:28 +0530

    Added profile avatar image for end user

--- app/helpers/participants/participant_accounts_helper.rb
+++ app/helpers/participants/participant_accounts_helper.rb
@@ -1,7 +1,7 @@
 module Participants::ParticipantAccountsHelper
 
   def get_profile_avatar_url
-    url = current_participant.avatar.present? ? current_participant.avatar.banner.url : "https://graph.facebook.com/105412254539529/picture?type=large"
+    url = current_participant.avatar.present? ? current_participant.avatar.banner.url : "end-user/profile_avatar.jpg"
     url
   end
 
