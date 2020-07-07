6b469c0eb9d6bb845a8a2619724e4868aed74ae0
Hardik Kanjariya <hardik@perksocial.com>
2020-07-07 19:15:05 +0530

    List All Eligibal Rewards for a Participant API

--- app/controllers/api/v1/rewards_controller.rb
+++ app/controllers/api/v1/rewards_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::RewardsController < Api::V1::BaseController
+  before_action :set_current_participant, only: :index, if: -> { @campaign.present? }
+
   ## Fetch All Rewards of a Campaign
   def index
-    render_success 200, true, 'Success', current_participant.as_json
+    rewards = @campaign.rewards.current_active.select {|x| x.available? }
+    render_success 200, true, 'Rewards fetched successfully.', rewards.as_json
   end
 
   ## Fetch Particular Reward Details
