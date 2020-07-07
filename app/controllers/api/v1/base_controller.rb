5c5887fc4d12dd9ff369fb14e5e5b660907000ae
Hardik Kanjariya <hardik@perksocial.com>
2020-07-07 18:57:50 +0530

    Custom Authentication Failure API Response Added

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,10 @@
-class Api::V1::BaseController < ActionController::Base
+class Api::V1::BaseController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :authenticate_participant!
+
+  ## Custom Authentication Error Message
+  def render_authenticate_error
+    return_error 401, false, 'You need to sign in or sign up before continuing.', {}
+  end
 end
