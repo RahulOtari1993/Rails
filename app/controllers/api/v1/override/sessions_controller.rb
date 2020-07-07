d9dd0189aead11ccbf207b89608830b37f4bbaf4
Hardik Kanjariya <hardik@perksocial.com>
2020-07-07 15:52:59 +0530

    Sign In API Implementation

--- app/controllers/api/v1/override/sessions_controller.rb
+++ app/controllers/api/v1/override/sessions_controller.rb
@@ -1,4 +1,63 @@
 class Api::V1::Override::SessionsController < DeviseTokenAuth::SessionsController
-  def new
+  def create
+    begin
+      param_hash = sign_in_params
+      validate_params param_hash
+      render_params_error unless validate_params param_hash
+
+      @resource = resource_class.where(organization_id: @organization.id, campaign_id: @campaign.id, email: param_hash[:email]).first
+
+      ## Check if Participant Exists or Not
+      render_auth_error unless @resource
+
+      if @resource.valid_password?(param_hash[:password])
+        # Create client id
+        @client_id = SecureRandom.urlsafe_base64(nil, false)
+        @token = SecureRandom.urlsafe_base64(nil, false)
+
+        @resource.tokens[@client_id] = {
+            token: BCrypt::Password.create(@token),
+            expiry: (Time.now + DeviseTokenAuth.token_lifespan).to_i
+        }
+
+        @resource.save
+        sign_in(:user, @resource, store: false, bypass: true)
+
+        render_success 200, true, 'Signed in successfully.', @resource.as_json
+      else
+        render_auth_error
+      end
+    rescue Exception => e
+      Rails.logger.info "ERROR: Sign In --> Message: #{e.message}"
+      handle_runtime_error
+    end
   end
+
+  private
+
+    ## Allows Participant Attributes
+    def sign_in_params
+      params.require(:participant).permit(:email, :password)
+    end
+
+    ## Validate Sign In API Params
+    def validate_params params_list
+      keys = params_list.keys
+      %w(email password).all? { |s| keys.include?(s) }
+    end
+
+    ## Render Params Error
+    def render_params_error
+      return return_error 500, false, 'Please pass required API params.', {}
+    end
+
+    ## Render Authentication Failed Error
+    def render_auth_error
+      return return_error 500, false, 'Invalid email or password.', {}
+    end
+
+    ## Handle Any Runtime Errors
+    def handle_runtime_error
+      return return_error 500, false, 'Oops. Service Unavailable, please try again after some time.', {}
+    end
 end
