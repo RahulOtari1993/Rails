27c66feb1fbac41eb5ff9b1c68ac4831f91355f2
Hardik Kanjariya <hardik@perksocial.com>
2020-07-08 13:14:24 +0530

    Include Question & It's Options in Fetch Particular Challenge API Response

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::ChallengesController < Api::V1::BaseController
   ## Fetch All Challenges of a Campaign
   def index
     challenges = @campaign.challenges.current_active.where.not(challenge_type: ['signup', 'connect']).select {|x| x.available? }
-    render_success 200, true, 'Challenges fetched successfully.', challenges.as_json
+    render_success 200, true, 'Challenges fetched successfully.', challenges.as_json(type: 'list')
   end
 
   ## Fetch Particular Challenge Details
   def show
     challenge = @campaign.challenges.where(id: params[:id])
-    render_success 200, true, 'Challenge details fetched successfully.', challenge.as_json
+    render_success 200, true, 'Challenge details fetched successfully.', challenge.as_json(type: 'one')
   end
 end
