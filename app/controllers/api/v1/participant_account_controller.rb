855a1eecded7cde49438199cad816f8fe33112ce
Hardik Kanjariya <hardik@perksocial.com>
2020-07-08 16:42:25 +0530

    Connect Facebook Account to Participant

--- app/controllers/api/v1/participant_account_controller.rb
+++ app/controllers/api/v1/participant_account_controller.rb
@@ -9,6 +9,16 @@ class Api::V1::ParticipantAccountController < Api::V1::BaseController
 
   ## Connect Facebook Account
   def facebook
+    render_params_error and return unless validate_facebook_params facebook_params
+
+    participant = current_participant
+    participant.assign_attributes(facebook_params)
+    participant.email = ""
+
+    participant.save(:validate => false)
+    participant.connect_challenge_completed('', '', 'connect', 'facebook')
+
+    render_success 200, true, 'Facebook account added successfully.', participant.as_json
   end
 
   ## Connect Twitter Account
@@ -18,4 +28,17 @@ class Api::V1::ParticipantAccountController < Api::V1::BaseController
   ## Disconnect Social Accounts (Facebook/Twitter)
   def disconnect
   end
+
+  private
+
+    ## Strong Params for Facebook
+    def facebook_params
+      params.require(:participant).permit(:facebook_uid, :facebook_token, :facebook_expires_at)
+    end
+
+    ## Validate Facebook API Params
+    def validate_facebook_params facebook_details
+      keys = facebook_details.keys
+      %w(facebook_uid facebook_token facebook_expires_at).all? { |s| keys.include?(s) }
+    end
 end
