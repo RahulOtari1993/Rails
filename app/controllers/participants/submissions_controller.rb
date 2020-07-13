5b1411d4e6a90fa13703d0bc3489505e28229b88
Hardik Kanjariya <hardik@perksocial.com>
2020-07-13 20:38:23 +0530

    Perform Encryption & Decryption Mechanism for Identifiers

--- app/controllers/participants/submissions_controller.rb
+++ app/controllers/participants/submissions_controller.rb
@@ -4,9 +4,17 @@ class Participants::SubmissionsController < ApplicationController
   def load_details
     if params[:type] == 'challenge' && params[:identifier].present?
       crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials[Rails.env.to_sym][:encryption_key])
-      encrypted_data = crypt.encrypt_and_sign('C9E392FF81C2fd9eef68649')
 
-      decrypted_back = crypt.decrypt_and_verify(encrypted_data)
+      ## Encrypt in URI Format & Pass in URL
+      # encrypted_data = crypt.encrypt_and_sign('C9E392FF81C29b00130abbfb')
+      # encrypted_data = URI.encode_www_form_component(encrypted_data)
+
+      identifier = crypt.decrypt_and_verify(params[:identifier])
+      participant_id = identifier[0, 12]
+      challenge_id  = identifier[12, 24]
+
+      participant = Participant.where(p_id: participant_id.to_s).first
+      challenge = Challenge.where(identifier: challenge_id.to_s).first
     end
   end
 end
