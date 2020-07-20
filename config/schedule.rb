c078f3ae0066388e8fd7a29ebffef010241793aa
Ranga_perk <ranga@perksocial.com>
2020-07-20 16:16:34 +0530

    Tesst cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,5 @@
-# env 'CRON_TZ', 'America/Los_Angeles'
-set :environment, :development
 set :output, "log/cron_log.log"
 
-every 1.minute do
+every 1.day, at: '10:55 am' do
   runner "Reward.sweepstack_cron"
 end
