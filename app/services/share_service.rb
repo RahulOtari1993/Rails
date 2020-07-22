45b16eb52b3003cd4a3e3fde7c72196bc6b935ab
Paul Cook <paul.pwc@gmail.com>
2020-07-22 17:53:06 -0400

    record points for unique visits to a share url

--- app/services/share_service.rb
+++ app/services/share_service.rb
@@ -1,13 +1,13 @@
 class ShareService
 
   def initialize
-
+    @processed_referral_ids = []
   end
 
   def process  participant, referral_ids, visit
     @participant = participant
     @referral_ids = referral_ids
-    processed_referral_ids = []
+    @processed_referral_ids = []
 
     referral_codes = ReferralCode.where(code: @referral_ids)
     referral_codes.each do |ref_code|
@@ -15,24 +15,10 @@ class ShareService
 
       if challenge.challenge_type == 'referral' and !participant.blank?
         # participant signed up so check if referral challenge has already been completed
-        actions = challenge.participant_actions.where(referred_participant_id: participant.id)
-
-        if actions.empty?
-          # Reward the referral points
-          action = challenge.participant_actions.create({
-            participant_id: ref_code.participant_id,
-            points: challenge.points,
-            referred_participant_id: participant.id,
-            action_type: 'recruit',
-            title: 'Recruit Sign Up',
-            ip_address: visit.ip,
-            campaign_id: challenge.campaign_id
-          })
-          if action.errors.empty?
-            challenge.update_attribute(:completions, challenge.completions+1)
-            processed_referral_ids << ref_code.code
-          end
-        end
+        process_referral ref_code, participant, challenge, visit
+      elsif challenge.challenge_type == 'share'
+        # don't need signed up participant to award points for shares
+        process_share ref_code, participant, challenge, visit
       else
         # Reward points for other share referrals
       end
@@ -91,5 +77,49 @@ class ShareService
       shortened_urls[platform] = "#{request.protocol}#{request.host}/s/#{short_url.unique_key}"
     end
     shortened_urls
+  end
+
+  private
+
+  def process_referral ref_code, participant, challenge, visit
+    actions = challenge.participant_actions.where(referred_participant_id: participant.id)
+
+    if actions.empty?
+      # Reward the referral points
+      action = challenge.participant_actions.create({
+        participant_id: ref_code.participant_id,
+        points: challenge.points,
+        referred_participant_id: participant.id,
+        action_type: 'recruit',
+        title: 'Recruit Sign Up',
+        ip_address: visit.ip,
+        campaign_id: challenge.campaign_id
+      })
+      if action.errors.empty?
+        challenge.update_attribute(:completions, challenge.completions+1)
+        @processed_referral_ids << ref_code.code
+      end
+    end
+  end
+
+  def process_share ref_code, participant, challenge, visit
+    actions = challenge.participant_actions.where(ahoy_visit_id: visit.id)
+
+    if actions.empty?
+      # Reward the referral points
+      action = challenge.participant_actions.create({
+        participant_id: ref_code.participant_id,
+        points: challenge.points,
+        ahoy_visit_id: visit.id,
+        action_type: 'share',
+        title: 'Visit to share challenge',
+        ip_address: visit.ip,
+        campaign_id: challenge.campaign_id
+      })
+      if action.errors.empty?
+        challenge.update_attribute(:completions, challenge.completions+1)
+        @processed_referral_ids << ref_code.code
+      end
+    end
   end
 end
