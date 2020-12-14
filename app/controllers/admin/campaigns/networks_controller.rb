10cb488ad6a9c3dca86bf65b0c9cc4c9139ed3a1
Hardik Kanjariya <hardik@perksocial.com>
2020-12-14 16:11:50 +0530

    Store Long Lived Token of Intagram

--- app/controllers/admin/campaigns/networks_controller.rb
+++ app/controllers/admin/campaigns/networks_controller.rb
@@ -50,15 +50,14 @@ class Admin::Campaigns::NetworksController < Admin::Campaigns::BaseController
 
   def instagram_callback
     Rails.logger.info "********************** Instagram Callback Initiated **********************"
-    user_agent = request.user_agent
-    remote_ip = request.remote_ip
-
 
     if @campaign.present? && @campaign.white_branding
       conf = CampaignConfig.where(campaign: @campaign.id).first
     else
       conf = GlobalConfiguration.first
     end
+
+    organization = @campaign.organization
 
     Rails.logger.info "*********** Configs: #{conf.inspect} *************"
     Rails.logger.info "*********** Fetch AUth Token From Here Onwards *************"
@@ -74,21 +73,63 @@ class Admin::Campaigns::NetworksController < Admin::Campaigns::BaseController
     Rails.logger.info "*********** TOKEN Response: #{response['access_token']} *************"
 
     if response.has_key?('access_token') && response.has_key?('user_id')
+      token_response = `curl https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=#{conf.instagram_app_secret}&access_token=#{response['access_token']}`
+      token_response = JSON.parse(token_response)
+
+      if token_response.has_key?('access_token') && response.has_key?('token_type') && response.has_key?('expires_in')
+        Rails.logger.info "*********** Long Token Details: #{token_response.inspect} *************"
+
+        ## Save the token response and user info details
+        network = @campaign.networks.where(organization_id: organization.id, platform: 'instagram').first_or_initialize
+
+        ## Update existing network or create a new Network
+        network.auth_token = token_response['access_token']
+        network.username = ''
+        network.expires_at = Time.now + (token_response['access_token'] / 3600 / 24).days
+        network.remote_avatar_url = ''
+        if network.save
+          flash[:notice] = 'Instagram account configuration successful.'
+          redirect_to admin_campaign_networks_path
+        else
+          flash[:notice] = 'Instagram account configuration failed.'
+          redirect_to admin_campaign_networks_path
+        end
+      else
+        flash[:notice] = (response.has_key?('error') && response['error'].has_key?('message')) ? response['error']['message'] : 'Instagram account configuration failed.'
+        redirect_to admin_campaign_networks_path
+      end
+
       # long_token = HTTParty.post("https://graph.instagram.com/access_token", body: {
       #   client_id: conf.instagram_app_id,
       #   client_secret: conf.instagram_app_secret,
       #   grant_type: 'ig_exchange_token',
       #   access_token: response['access_token']
       # })
-
-
-      long_token = HTTParty.get("https://api.instagram.com/oauth/access_token?grant_type=ig_exchange_token&client_secret=#{conf.instagram_app_secret}&access_token=#{response['access_token']}")
-
-
-
-      Rails.logger.info "*********** long_token: #{long_token.inspect} *************"
-
-      redirect_to admin_campaign_networks_path
+      #
+      # { client_secret: '67c4da5924dae9253b4f10fdef1e8eda', grant_type: 'ig_exchange_token', access_token: 'IGQVJXQ2lJU0NPNEV6Vktkbk5EYzhyLWdmSU9iQjRQU1VHNGduamV2VEFVd1dVWXdiUDdMekVxanA5YUExdHAxRllCMDI5VVR0ZA3RMVTl5QnNUYmczb3ViazJqdnowc1REUjRDSUhrc1RzRXdCZAUJyTkZApVnptb1JlejdJ' }
+      #
+      # ## Purl Curl Request
+      # uri = URI.parse("https://api.instagram.com/oauth/access_token?grant_type=ig_exchange_token&client_secret=67c4da5924dae9253b4f10fdef1e8eda&access_token=IGQVJXQ2lJU0NPNEV6Vktkbk5EYzhyLWdmSU9iQjRQU1VHNGduamV2VEFVd1dVWXdiUDdMekVxanA5YUExdHAxRllCMDI5VVR0ZA3RMVTl5QnNUYmczb3ViazJqdnowc1REUjRDSUhrc1RzRXdCZAUJyTkZApVnptb1JlejdJ")
+      # response = Net::HTTP.get_response(uri)
+      #
+      # ##
+      # HTTParty.post("https://api.instagram.com/oauth/access_token", body: { client_id: '426018228762844', client_secret: '67c4da5924dae9253b4f10fdef1e8eda', grant_type: 'ig_exchange_token', access_token: 'IGQVJXQ2lJU0NPNEV6Vktkbk5EYzhyLWdmSU9iQjRQU1VHNGduamV2VEFVd1dVWXdiUDdMekVxanA5YUExdHAxRllCMDI5VVR0ZA3RMVTl5QnNUYmczb3ViazJqdnowc1REUjRDSUhrc1RzRXdCZAUJyTkZApVnptb1JlejdJ' })
+      # long_token = HTTParty.get("https://api.instagram.com/oauth/access_token?grant_type=ig_exchange_token&client_secret=#{conf.instagram_app_secret}&access_token=#{response['access_token']}", :headers => {"Content-Type" => "application/json"})
+      #
+      # long_token = HTTParty.get("https://api.instagram.com/oauth/access_token?grant_type=ig_exchange_token&client_secret=67c4da5924dae9253b4f10fdef1e8eda&access_token=IGQVJXQ2lJU0NPNEV6Vktkbk5EYzhyLWdmSU9iQjRQU1VHNGduamV2VEFVd1dVWXdiUDdMekVxanA5YUExdHAxRllCMDI5VVR0ZA3RMVTl5QnNUYmczb3ViazJqdnowc1REUjRDSUhrc1RzRXdCZAUJyTkZApVnptb1JlejdJ", :headers => {"Content-Type" => "application/json"}, format: :json)
+      #
+      # long_token = HTTParty.get("https://api.instagram.com/oauth/access_token", query: { grant_type: 'ig_exchange_token', client_secret: '67c4da5924dae9253b4f10fdef1e8eda', access_token: 'IGQVJXQ2lJU0NPNEV6Vktkbk5EYzhyLWdmSU9iQjRQU1VHNGduamV2VEFVd1dVWXdiUDdMekVxanA5YUExdHAxRllCMDI5VVR0ZA3RMVTl5QnNUYmczb3ViazJqdnowc1REUjRDSUhrc1RzRXdCZAUJyTkZApVnptb1JlejdJ'}, :headers => {"Content-Type" => "application/json"})
+      #
+      # long_token = HTTParty.get("https://api.instagram.com/oauth/access_token", query: { grant_type: 'ig_exchange_token', client_secret: '67c4da5924dae9253b4f10fdef1e8eda', access_token: 'IGQVJXQ2lJU0NPNEV6Vktkbk5EYzhyLWdmSU9iQjRQU1VHNGduamV2VEFVd1dVWXdiUDdMekVxanA5YUExdHAxRllCMDI5VVR0ZA3RMVTl5QnNUYmczb3ViazJqdnowc1REUjRDSUhrc1RzRXdCZAUJyTkZApVnptb1JlejdJ'})
+      #
+      #
+      # ## Faraday Gem
+      # resp = Faraday.get("https://api.instagram.com/oauth/access_token", {client_secret: '67c4da5924dae9253b4f10fdef1e8eda', grant_type: 'ig_exchange_token', access_token: 'IGQVJXQ2lJU0NPNEV6Vktkbk5EYzhyLWdmSU9iQjRQU1VHNGduamV2VEFVd1dVWXdiUDdMekVxanA5YUExdHAxRllCMDI5VVR0ZA3RMVTl5QnNUYmczb3ViazJqdnowc1REUjRDSUhrc1RzRXdCZAUJyTkZApVnptb1JlejdJ'}, {'Accept' => 'text/html', 'Content-Type' => 'text/html'})
+      #
+      # resp = Faraday.get("https://api.instagram.com/oauth/access_token") do |req|
+      #   req.headers['Content-Type'] = 'application/json'
+      #   req.body = { client_secret: '67c4da5924dae9253b4f10fdef1e8eda', grant_type: 'ig_exchange_token', access_token: 'IGQVJXQ2lJU0NPNEV6Vktkbk5EYzhyLWdmSU9iQjRQU1VHNGduamV2VEFVd1dVWXdiUDdMekVxanA5YUExdHAxRllCMDI5VVR0ZA3RMVTl5QnNUYmczb3ViazJqdnowc1REUjRDSUhrc1RzRXdCZAUJyTkZApVnptb1JlejdJ' }.to_json
+      # end
     else
       flash[:notice] = response.has_key?('error_message') ? response['error_message'] : 'Instagram account configuration failed.'
       redirect_to admin_campaign_networks_path
@@ -99,5 +140,9 @@ class Admin::Campaigns::NetworksController < Admin::Campaigns::BaseController
 
   def set_network
     @network = @campaign.networks.find(params[:id])
+  end
+
+  def fetch_long_lived_token
+
   end
 end
