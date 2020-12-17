24c6fabc4795631b7169bbde595cca6c4063e762
Hardik Kanjariya <hardik@perksocial.com>
2020-12-17 18:00:16 +0530

    Facebook Social Feed Batch Job Code Refactoring

--- app/services/facebook_social_feed_service.rb
+++ app/services/facebook_social_feed_service.rb
@@ -5,129 +5,87 @@ class FacebookSocialFeedService
     @network = @campaign.networks.where(id: network_id).first rescue nil
   end
 
-  ## Fetch facebook feeds for specific network
+  ## Fetch Facebook Feeds
   def process
-    if @organization.present? && @campaign.present? && @network.present?
-
+    if @organization.present? && @campaign.present? && @network.present? && @network.auth_token.present?
       ## Check whether Facebook Social Feed Challenge is Active & Available
       facebook_media_challenge = @campaign.challenges.current_active.where(challenge_type: 'engage', parameters: 'facebook').first
       if facebook_media_challenge.present?
-        ## fetch client auth token and facebook app secret
-        auth_token = @network.auth_token
-        facebook_app_secret = (@campaign.present? && @campaign.white_branding) ? CampaignConfig.where(campaign_id: @campaign.id).first.facebook_app_secret : GlobalConfiguration.first.facebook_app_secret
+        ## Fetch client auth token and facebook app secret
+        if @campaign.present? && @campaign.white_branding
+          conf = CampaignConfig.where(campaign_id: @campaign.id).first
+        else
+          conf = GlobalConfiguration.first
+        end
 
-        ## initialize the connected facebook client
-        client = Koala::Facebook::API.new(auth_token, facebook_app_secret)
+        if conf.present? && conf.facebook_app_secret.present?
+          ## Initialize Facebook Client
+          client = Koala::Facebook::API.new(@network.auth_token, conf.facebook_app_secret)
 
-        ## To fetch facebook pages and insert records
-        page_records = client.get_connections('me', 'accounts')
-        page_records.each do |page_record|
+          ## To fetch facebook pages and insert records
+          page_records = client.get_connections('me', 'accounts')
+          page_records.each do |page_record|
+            Rails.logger.info "******** Fetch Facebook feeds for Page: #{page_record['name']} -- Start ********"
 
-          Rails.logger.info "******** Fetch Facebook feeds for Page: #{page_record['name']} -- Start ********"
+            ## Find Existing Facebook Page or Create a New One
+            network_page = @network.network_pages.find_or_initialize_by(page_id: page_record['id'])
+            network_page.update_attributes({
+                                             page_name: page_record['name'],
+                                             category: page_record['category'],
+                                             page_access_token: page_record['access_token'],
+                                             network_id: @network.id,
+                                             campaign_id: @campaign.id
+                                           })
 
-          network_page = @network.network_pages.where(page_id: page_record['id']).first
-          if network_page.present?
-            network_page.page_name = page_record['name']
-            network_page.category = page_record['category']
-            network_page.page_access_token = page_record['access_token']
-            network_page.save
-          else
-            network_page = @campaign.network_pages.new(
-              page_id: page_record['id'],
-              page_name: page_record['name'],
-              category: page_record['category'],
-              page_access_token: page_record['access_token'],
-              network_id: @network.id
-            )
-            network_page.save
-          end
+            ## Fetch Posts from Specific Facebook Page
+            page_posts = client.get_connection(network_page.page_id, 'posts')
+            page_posts.each do |page_post|
+              ## Find Existing Page Post or Create a New One
+              network_page_post = network_page.network_page_posts.find_or_initialize_by(network_id: @network.id, post_id: page_post['id'])
+              network_page_post.update_attributes({
+                                                    message: page_post['message'],
+                                                    network_page_id: network_page.id,
+                                                    created_time: page_post['created_time']
+                                                  })
 
-          ## To fetch facebook specific page posts and insert records
-          page_posts = client.get_connection(network_page.page_id, 'posts')
-          page_posts.each do |page_post|
+              ## Fetch Post Attachments && Update Page Post Details
+              attachments_hash = client.get_connections(network_page_post.post_id, 'attachments')[0]
+              network_page_post.update({
+                                         title: attachments_hash['title'],
+                                         post_type: attachments_hash['type'] == 'video_inline' ? 'video' : attachments_hash['type'],
+                                         url: attachments_hash['url']
+                                       })
 
-            network_page_post = network_page.network_page_posts.where(network_id: @network.id, post_id: page_post['id']).first
-
-            if network_page_post.present?
-              network_page_post.message = page_post['message']
-              network_page_post.created_time = page_post['created_time']
-              network_page_post.save
-            else
-              network_page_post = network_page.network_page_posts.new(
-                post_id: page_post['id'],
-                message: page_post['message'],
-                network_id: @network.id,
-                network_page_id: network_page['id'],
-                created_time: page_post['created_time']
-              )
-              network_page_post.save
-            end
-
-            ## To fetch facebook specific post attachments and insert records
-            post_attachments_data_hash = client.get_connections(network_page_post.post_id, 'attachments')[0]
-
-            ## update page post details
-            network_page_post.title = post_attachments_data_hash['title']
-            network_page_post.post_type = post_attachments_data_hash['type']
-            network_page_post.url = post_attachments_data_hash['url']
-            network_page_post.save
-
-            ## insert post attachments
-            if post_attachments_data_hash['subattachments'].present?
-              post_attachments = post_attachments_data_hash['subattachments']['data']
-              post_attachments.each do |post_attachment|
-                net_page_post_att = network_page_post.network_page_post_attachments.where(category: post_attachment['type'], url: post_attachment['url']).first
-                att_type_key = post_attachment['media'].keys[0]
-
-                if net_page_post_att.present?
-                  net_page_post_att.height = post_attachment['media'][att_type_key]['height']
-                  net_page_post_att.width = post_attachment['media'][att_type_key]['width']
-                  net_page_post_att.media_src = post_attachment['media'][att_type_key]['src']
-                  net_page_post_att.media_type = post_attachment['media'].keys[0]
-                  net_page_post_att.target = post_attachment['target']
-                  net_page_post_att.save
-                else
-                  net_page_post_att = network_page_post.network_page_post_attachments.new(
-                    height: post_attachment['media'][att_type_key]['height'],
-                    width: post_attachment['media'][att_type_key]['width'],
-                    media_src: post_attachment['media'][att_type_key]['src'],
-                    media_type: post_attachment['media'].keys[0],
-                    category: post_attachment['type'],
-                    url: post_attachment['url'],
-                    target: post_attachment['target']
-                  )
-                  net_page_post_att.save
+              if attachments_hash['subattachments'].present?
+                post_attachments = attachments_hash['subattachments']['data']
+                post_attachments.each do |post_attachment|
+                  ## Find Existing Network Page Post or Create a New One
+                  att_type_key = post_attachment['media'].keys[0]
+                  category = post_attachment['type'] == 'video_inline' ? 'video' : post_attachment['type']
+                  post = network_page_post.network_page_post_attachments.find_or_initialize_by(category: category, url: post_attachment['url'])
+                  post.update_attributes({
+                                           height: post_attachment['media'][att_type_key]['height'],
+                                           width: post_attachment['media'][att_type_key]['width'],
+                                           target: post_attachment['target'],
+                                           image_source: post_attachment['media'][att_type_key]['src'],
+                                           video_source: attachments_hash['media']['source']
+                                         })
                 end
-              end
-            else
-              net_page_post_att = network_page_post.network_page_post_attachments.where(category: post_attachments_data_hash['type'], url: post_attachments_data_hash['url']).first
-              att_type_key = post_attachments_data_hash['media'].keys[0]
-              if net_page_post_att.present?
-                net_page_post_att.height = post_attachments_data_hash['media'][att_type_key]['height']
-                net_page_post_att.width = post_attachments_data_hash['media'][att_type_key]['width']
-                net_page_post_att.media_src = post_attachments_data_hash['media'][att_type_key]['src']
-                net_page_post_att.media_type = post_attachments_data_hash['media'].keys[0]
-                net_page_post_att.video_source = post_attachments_data_hash['media']['source']
-                net_page_post_att.target = post_attachments_data_hash['target']
-                net_page_post_att.save
               else
-                net_page_post_att = network_page_post.network_page_post_attachments.new(
-                  height: post_attachments_data_hash['media'][att_type_key]['height'],
-                  width: post_attachments_data_hash['media'][att_type_key]['width'],
-                  media_src: post_attachments_data_hash['media'][att_type_key]['src'],
-                  media_type: post_attachments_data_hash['media'].keys[0],
-                  video_source: post_attachments_data_hash['media']['source'],
-                  category: post_attachments_data_hash['type'],
-                  url: post_attachments_data_hash['url'],
-                  target: post_attachments_data_hash['target']
-                )
-                net_page_post_att.save
+                att_type_key = attachments_hash['media'].keys[0]
+                category = attachments_hash['type'] == 'video_inline' ? 'video' : attachments_hash['type']
+                post = network_page_post.network_page_post_attachments.find_or_initialize_by(category: category, url: attachments_hash['url'])
+                post.update_attributes({
+                                         height: attachments_hash['media'][att_type_key]['height'],
+                                         width: attachments_hash['media'][att_type_key]['width'],
+                                         target: attachments_hash['target'],
+                                         image_source: attachments_hash['media'][att_type_key]['src'],
+                                         video_source: attachments_hash['media']['source']
+                                       })
               end
             end
-
+            Rails.logger.info "******** Fetch Facebook feeds for Page: #{page_record['name']} -- end ********"
           end
-
-          Rails.logger.info "******** Fetch Facebook feeds for Page: #{page_record['name']} -- end ********"
         end
       end
     end
