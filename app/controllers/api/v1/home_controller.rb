e564754a6d179bb31b5811c85245fdc8186154ae
Ranga_perk <ranga@perksocial.com>
2021-01-08 13:09:52 +0530

    changed query for featured challenges for home screen api

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -6,10 +6,13 @@ class Api::V1::HomeController < Api::V1::BaseController
     challenges = @campaign.challenges
     rewards = @campaign.rewards
 
-    featured_challenges = challenges.featured.where.not(challenge_type: ['signup', 'connect']).select { |x| x.available? }
-    ## fetch current active upcoming challenge
     submitted_challenge_ids = @campaign.submissions.where(participant_id: current_participant.id).pluck(:challenge_id)
-    current_challenge = challenges.current_active.where.not(id: submitted_challenge_ids).where.not(challenge_type: ['signup', 'connect']).select { |x| x.available? && (x.start.strftime('%H:%M:%S') > DateTime.now.strftime('%H:%M:%S')) }.sort_by{ |x| x.start.strftime('%H:%M:%S') }.first
+
+    ## Featured Challenges
+    featured_challenges = challenges.current_active.featured.where.not(id: submitted_challenge_ids).where(challenge_type: 'article').select { |x| x.available? }
+
+    ## fetch current active upcoming challenge
+    current_challenge = challenges.current_active.where.not(id: submitted_challenge_ids).where.not(challenge_type: ['signup', 'connect', 'article']).select { |x| x.available? && (x.start.strftime('%H:%M:%S') > DateTime.now.strftime('%H:%M:%S')) }.sort_by{ |x| x.start.strftime('%H:%M:%S') }.first
 
     ## fetch current active upcoming reward
     claimed_reward_ids = @campaign.participant_actions.where(participant_id: current_participant.id, actionable_type: "Reward").pluck(:actionable_id).uniq
