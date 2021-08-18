bed1b597a1f5934bb3afacceac1e5432d50cb00f
Hardik Kanjariya <hardik@perksocial.com>
2021-08-18 16:14:59 +0530

    Facebook OmniAuth Lib Updates

--- app/lib/facebook_omniauth_setup.rb
+++ app/lib/facebook_omniauth_setup.rb
@@ -1,13 +1,14 @@
 class FacebookOmniauthSetup
   # OmniAuth expects the class passed to setup to respond to the #call method.
   # env - Rack environment
-  def self.call(env)
+  def self.call(env)    
     new(env).setup
   end
 
   # Assign variables and create a request object for use later.
   # env - Rack environment
   def initialize(env)
+    Rails.logger.info "============== IN initialize =============="
     @env = env
     @request = ActionDispatch::Request.new(env)
   end
@@ -16,11 +17,13 @@ class FacebookOmniauthSetup
 
   # The main purpose of this method is to set the consumer key and secret.
   def setup
+    Rails.logger.info "============== IN SETUP =============="
     @env['omniauth.strategy'].options.merge!(custom_credentials)
   end
 
   # Use the subdomain in the request to find the account with credentials
   def custom_credentials   
+    Rails.logger.info "============== IN LIB =============="
     conf = GlobalConfiguration.first
 
     {
