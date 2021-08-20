9ce09e9f4ff056029d8dc4bf3cb23580fb2d2c3f
Hardik Kanjariya <hardik@perksocial.com>
2021-08-20 20:24:59 +0530

    Display Template Editor on Page Load

--- app/assets/javascripts/campaign_admin/template.js
+++ app/assets/javascripts/campaign_admin/template.js
@@ -1,4 +1,7 @@
 $(document).on('turbolinks:load', function() {
   // Color Picker Integration
   $('.colour-picker-txt').minicolors({theme: 'bootstrap'})
+
+  // Open Template Editor on Page Load
+  $('.action-edit').trigger('click');
 });
