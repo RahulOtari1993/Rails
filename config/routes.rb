80a3af674b819ae8a4b2a6e2f97347889de5ae97
RahulOtari1993 <rahulotari2393@gmail.com>
2022-03-10 18:11:43 +0530

    added business model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   root 'default#dashboard'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+## Business Routes
+  resources :businesses do
+    collection do
+      get '/fetch_businesses', to: 'businesses#fetch_businesses'
+    end
+  end 
 end
